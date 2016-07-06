fc25ab14ea4bafed5ab997e45a484fd0eb6ff680
Teis Draiby <teis@teis.net>
2016-07-06 21:06:25 +0200

    Add root page from original BoatCam project

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// CONFIG
+app.locals.title = "BoatCam";
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
