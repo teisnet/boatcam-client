d2b51804f6a8f09403dc34b5d5ffe6bafbefe5eb
Teis Draiby <teis@teis.net>
2016-07-07 18:46:45 +0200

    Add /admin/cameras page from original BoatCam project

--- routes/admin.js
+++ routes/admin.js
@@ -6,5 +6,8 @@ router.get('/', function(req, res) {
 	res.render('admin/index' /*, { user: req.user }*/);
 });
 
+router.get('/cameras', function(req, res) {
+	res.render('admin/cameras');
+});
 
 module.exports = router;
