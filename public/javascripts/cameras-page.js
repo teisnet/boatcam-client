0b28405b94b828488d44d0c2878ffd0c8874ed3d
Teis Draiby <teis@teis.net>
2016-08-16 09:37:01 +0200

    Fix initial camera status update
    If Cameras.getAll has not returned before first websocket status
    the cameras will not show their status

--- public/javascripts/cameras-page.js
+++ public/javascripts/cameras-page.js
@@ -6,13 +6,13 @@ var Camera = BoatCamApi.cameras; //new Api("cameras");
 Camera.getAll()
 .done(function(cameras) {
 	$(cameras).each(function (index, camera) {
-		var listItem = '<li id="'+ camera._id + '"><a href="cameras/' + camera.slug + '"><div class="status-indicator"></div>' + camera.title + "</a></li>";
+		var listItem = '<li id="'+ camera._id + '"><a href="cameras/' + camera.slug + '"><div class="status-indicator ' + camera.status.status + '"></div>' + camera.title + "</a></li>";
 		$("ul.camera-list").append(listItem);
 	});
-});
 
-socket.on("status", function(status){
-	$.isArray(status) ? status.forEach(function(value) { setStatus(value); }) : setStatus(status);
+	socket.on("status", function(status){
+		$.isArray(status) ? status.forEach(function(value) { setStatus(value); }) : setStatus(status);
+	});
 });
 
 function setStatus(status) {
